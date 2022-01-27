65f8a377507efe206d4daf9895c293b976af0429
RoyMillan <millan.rodrigo201@gmail.com>
2022-01-26 19:04:59 -0600

    add introduction to pyhton

---
+++ Introduction/variables.py
@@ -0,0 +1,19 @@
+# nombre_variable = valor_variable
+
+# tipo string o cadena
+nombre = 'Rodrigo'
+print(nombre)
+print(f'tipo de datos: {type(nombre)}')
+
+# tipo entero 
+numero = 26
+print(numero)
+print(f'tipo de datos: {type(numero)}')
+
+#tipo flotante 
+flotante = 3.14
+print(flotante)
+print(f'tipo de datos: {type(flotante)}')
+
+# Asignación multiple de variables, sirve para ahorrar espacio en memoria
+nombre, apellido = 'Rodrigo', 'Millan'
---
+++ Introduction/variables_constantes.py
@@ -0,0 +1,10 @@
+# Variables 
+cantidad = 1000
+print(cantidad)
+cantidad = cantidad + 1500
+print(cantidad)
+
+# Constantes 
+    # las variables escritas en mayuscula significa que son constantes, python no reconoce como constantes
+PI = 3.14
+print(PI)
